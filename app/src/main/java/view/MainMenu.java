f6ecbd3547e6461a4751a7be99014f5ed3c23b0e
Ittai <ittai1609@gmail.com>
2022-02-04 10:43:45 +0200

    V_1

--- app/src/main/java/view/MainMenu.java
+++ app/src/main/java/view/MainMenu.java
@@ -1,13 +1,24 @@
 package view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.TestLooperManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.thebigproject.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class MainMenu extends AppCompatActivity {
 
@@ -15,6 +26,13 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+
+
+        Intent i = getIntent();
+        String userName = i.getStringExtra("userName");
+        TextView textView = findViewById(R.id.userName);
+        textView.setText(userName);
 
         Button beckBuilder = findViewById(R.id.DeckBuilder);
         Button shop = findViewById(R.id.Shop);
@@ -46,4 +64,5 @@ public class MainMenu extends AppCompatActivity {
 
 
     }
+
 }
